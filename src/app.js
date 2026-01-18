3ca61cdc1d1f49e474552756f6cefadf85f88b39
vyrriox <vyrriox@gmail.com>
2026-01-18 13:38:04 +0100

    1.2.1

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ if (dev) {
     app.setPath('appData', appdata)
 }
 
+app.userAgentFallback = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
+
 let store = null;
 
 if (!app.requestSingleInstanceLock()) app.quit();
